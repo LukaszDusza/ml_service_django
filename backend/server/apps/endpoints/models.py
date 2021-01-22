81e0f8b78d2552befcb0a1fac6d0e8a4c4649d9a
Lukasz Dusza <lukaszdusza280@gmail.com>
2021-01-22 20:49:10 +0100

    in progress

--- backend/server/apps/endpoints/models.py
+++ backend/server/apps/endpoints/models.py
@@ -27,4 +27,4 @@ class MLRequest(models.Model):
     response = models.CharField(max_length=10000)
     feedback = models.CharField(max_length=10000, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    parent_endpoint = models.ForeignKey(Endpoint, on_delete= models.CASCADE)
+    parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE)
--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -28,7 +28,7 @@ try:
     registry.add_algorithm(endpoint_name="income_classifier",
                             algorithm_object=rf,
                             algorithm_name="random forest",
-                            algorithm_status="production",
+                            algorithm_status="ab_testing",
                             algorithm_version="0.0.1",
                             owner="Lukasz",
                             algorithm_description="Random Forest with simple pre- and post-processing",
